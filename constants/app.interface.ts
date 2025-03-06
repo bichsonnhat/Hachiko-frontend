ba6836b4c8b84cca7e5bd9f75123e983fc903605
nhdhieuu <146748393+nhdhieuu@users.noreply.github.com>
2025-03-06 21:35:42 +0700

    Feature/shop (#6)
    * feat: add shop screen
    
    * feat: add shop modal
    
    * chore: update list data

--- constants/app.interface.ts
+++ constants/app.interface.ts
@@ -1,15 +1,26 @@
-import { ImageSourcePropType } from "react-native";
+import {ImageSourcePropType} from "react-native";
 
 export interface DrinkOrder {
-  drink_price: number;
-  drink_name: string;
-  drink_note: string;
-  drink_quantity: number;
+    drink_price: number;
+    drink_name: string;
+    drink_note: string;
+    drink_quantity: number;
 };
 
 export interface DrinkPropertie {
-  drink_img: ImageSourcePropType;
-  drink_name: string;
-  drink_price: number;
-  drink_description: string;
+    drink_img: ImageSourcePropType;
+    drink_name: string;
+    drink_price: number;
+    drink_description: string;
+}
+
+export interface Store {
+    id: number;
+    name: string;
+    address: string;
+    distance: number;
+    open_time: Date;
+    close_time: Date;
+    image: string;
+
 }
