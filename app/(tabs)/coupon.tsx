860256239b7240c4224fe753db5b12543fc6301b
nhdhieuu <22520439@gm.uit.edu.vn>
2025-03-12 03:07:58 +0700

    feat: add My voucher Screen

--- app/(tabs)/coupon.tsx
+++ app/(tabs)/coupon.tsx
@@ -5,15 +5,17 @@ import {Colors} from "@/constants/Colors";
 import {Ticket} from "lucide-react-native";
 import React, {useState} from "react";
 import VoucherDetailModal from "@/components/VoucherScreen/VoucherDetailModal";
+import { useRouter} from "expo-router";
 
 export default function CouponScreen() {
+    const router = useRouter();
     const [modalVisible, setModalVisible] = useState(false);
 
     const imgUri = "https://firebasestorage.googleapis.com/v0/b/thehachikocoffee-aed51.appspot.com/o/Voucher%2FGi%E1%BA%A3m%2030%25%20%2B%20Freeship%20%C4%90%C6%A1n%20T%E1%BB%AB%203%20Ly.jpg?alt=media&token=72f401b9-4334-48eb-aea8-cdf05a815c77";
     const safeHeight = StatusBar.currentHeight || 0;
     return (
         <SafeAreaView style={{flex: 1, marginTop: safeHeight}}>
-            <View className={"px-4 bg-amber-500"}>
+            <View className={"px-4 bg-[#ff9123]"}>
                 <View className={"flex-row py-2 items-center"}>
                     <Text className={"flex-1 text-xl font-bold text-white"}>Ưu đãi</Text>
                     <TouchableOpacity>
@@ -28,18 +30,18 @@ export default function CouponScreen() {
 
             </View>
             <View className={"bg-gray-100 px-4 flex-row py-2 items-center"}>
-                <Text className={"flex-1 text-xl font-bold"}>Khuyến mãi</Text>
-                <TouchableOpacity>
-                    <View className={`flex-row h-12 items-center bg-white rounded-full px-4 py-2 `}>
-                        <Text className={'font-bold ml-2 text-[#E47905] text-lg'}>Xem tất cả</Text>
+                <Text className={"flex-1 text-xl font-bold"}>Sắp hết hạn</Text>
+                <TouchableOpacity onPress={() => router.push('/voucher/my-voucher')}>
+                    <View className={`flex-row h-12 items-center bg-[#fff7e3] rounded-full px-4 py-2 `}>
+                        <Text className={'font-bold ml-2 text-[#ffa145] text-lg'}>Xem tất cả</Text>
                     </View>
                 </TouchableOpacity>
             </View>
             <View className={"flex-row py-2 items-center bg-gray-100 px-4 "}>
-                <Text className={"flex-1 text-xl font-bold"}>Khuyến mãi</Text>
-                <TouchableOpacity>
-                    <View className={`flex-row h-12 items-center bg-white rounded-full px-4 py-2 `}>
-                        <Text className={'font-bold ml-2 text-[#E47905] text-lg'}>Xem tất cả</Text>
+                <Text className={"flex-1 text-xl font-bold"}>Voucher của bạn</Text>
+                <TouchableOpacity onPress={() => router.push('/voucher/my-voucher')}>
+                    <View className={`flex-row h-12 items-center bg-[#fff7e3] rounded-full px-4 py-2 `}>
+                        <Text className={'font-bold ml-2 text-[#ffa145] text-lg'}>Xem tất cả</Text>
                     </View>
                 </TouchableOpacity>
             </View>
