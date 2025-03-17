b203c68cd530a2e3af688acc54b8f7ff3267b41a
h0angpc <118001661+h0angpc@users.noreply.github.com>
2025-03-17 12:43:14 +0700

    refactor: move code input-form to dashboard folder and routing

--- app/(dashboard)/user/create.tsx
+++ app/(dashboard)/user/create.tsx
@@ -1,8 +1,8 @@
-import {View,Text} from "react-native";
-import {useNavigation} from "expo-router";
-import {useEffect} from "react";
+import { View, Text } from "react-native";
+import { useNavigation } from "expo-router";
+import { useEffect } from "react";
 
-export default function CreateUser (){
+export default function CreateUser() {
     const navigation = useNavigation();
     useEffect(() => {
         navigation.setOptions({
