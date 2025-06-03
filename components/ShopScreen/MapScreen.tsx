0e263973a168edd772f18930162e712d4ce95195
nhdhieuu <22520439@gm.uit.edu.vn>
2025-06-03 23:44:16 +0700

    feat: map store api

--- components/ShopScreen/MapScreen.tsx
+++ components/ShopScreen/MapScreen.tsx
@@ -13,15 +13,19 @@ interface ShopModalProps {
     visible: boolean;
     onClose: () => void;
 }
+
 const COLORS = {
     green: '#00FF00',
     red: '#FF0000',
     blue: '#0000FF'
 };
+
+
+
 export const MapScreen = ({visible, onClose}: ShopModalProps) => {
     const [location, setLocation] = useState<Location.LocationObject | null>(null);
     const [errorMsg, setErrorMsg] = useState<string | null>(null);
-    const { errorMessage, callApi: callStoreApi } = useApi<void>()
+    const {errorMessage, callApi: callStoreApi} = useApi<void>()
     const [stores, setStores] = useState<IStore[]>([]);
     const [selectedStore, setSelectedStore] = useState<IStore | null>(null);
     const [coordinates, setCoordinates] = useState<any[]>([]);
@@ -33,20 +37,18 @@ export const MapScreen = ({visible, onClose}: ShopModalProps) => {
             const response = await axios.get(url);
             const encodedPolyline = response.data.routes[0].geometry;
             const decodedCoordinates = polyline.decode(encodedPolyline);
-            console.log(decodedCoordinates)
             // @ts-ignore
             const formattedCoordinates = decodedCoordinates.map(coordPair => {
-                return { latitude: coordPair[0], longitude: coordPair[1] };
+                return {latitude: coordPair[0], longitude: coordPair[1]};
             });
             setCoordinates(formattedCoordinates);
-
 
 
         } catch (error) {
             console.log('Error fetching route:', error);
             setCoordinates([
-                { latitude: fromLat, longitude: fromLong },
-                { latitude: toLat, longitude: toLong }
+                {latitude: fromLat, longitude: fromLong},
+                {latitude: toLat, longitude: toLong}
             ]);
         }
     };
@@ -54,8 +56,7 @@ export const MapScreen = ({visible, onClose}: ShopModalProps) => {
     useEffect(() => {
         const fetchStores = async () => {
             await callStoreApi(async () => {
-                const { data } = await apiService.get<IStore[]>("/stores");
-                console.log("Fetched stores:", data);
+                const {data} = await apiService.get<IStore[]>("/stores");
                 setStores(data);
             });
         };
@@ -65,14 +66,13 @@ export const MapScreen = ({visible, onClose}: ShopModalProps) => {
 
     useEffect(() => {
         async function getCurrentLocation() {
-            let { status } = await Location.requestForegroundPermissionsAsync();
+            let {status} = await Location.requestForegroundPermissionsAsync();
             if (status !== 'granted') {
                 setErrorMsg('Permission to access location was denied');
                 return;
             }
 
             let location = await Location.getCurrentPositionAsync({});
-            console.log(location);
             setLocation(location);
         }
 
@@ -132,14 +132,15 @@ export const MapScreen = ({visible, onClose}: ShopModalProps) => {
                             onCalloutPress={() => console.log(selectedStore?.id)}
                         >
                             <Callout>
-                                <View style={{ alignItems: 'center', width: 150, height: 380,
+                                <View style={{
+                                    alignItems: 'center', width: 150, height: 380,
                                 }}>
                                     <Image
-                                        source={{ uri: store.imageURL }}
-                                        style={{ width: 100, height: 60, borderRadius: 8, marginBottom: 4 }}
+                                        source={{uri: store.imageURL}}
+                                        style={{width: 100, height: 60, borderRadius: 8, marginBottom: 4}}
                                         resizeMode="cover"
                                     />
-                                    <Text style={{ fontWeight: 'bold' }}>{store.name}</Text>
+                                    <Text style={{fontWeight: 'bold'}}>{store.name}</Text>
                                 </View>
                             </Callout>
                         </Marker>
@@ -155,7 +156,7 @@ export const MapScreen = ({visible, onClose}: ShopModalProps) => {
                 </MapView>
 
                 <View style={styles.buttonContainer}>
-                    <Button title="Tắt map" onPress={onClose} />
+                    <Button title="Tắt map" onPress={onClose}/>
                 </View>
             </View>
         </Modal>
--- components/ShopScreen/ShopModal.tsx
+++ components/ShopScreen/ShopModal.tsx
@@ -2,29 +2,29 @@ import React from "react";
 import { View, Text, Image, TouchableOpacity, Modal, Dimensions } from "react-native";
 import { Locate, Navigation } from "lucide-react-native";
 import Slider from "@/components/HomeScreen/Slider";
-import { Store } from "@/constants";
+import {IStore, Store} from "@/constants";
 
 interface ShopModalProps {
-    store?: Store;
+    store?: IStore;
     visible: boolean;
     onClose: () => void;
 }
 const { height } = Dimensions.get('window');
 
 const ShopModal: React.FC<ShopModalProps> = ({ visible, onClose, store }) => {
-    const formattedOpenTime = store?.open_time.toLocaleTimeString("vi-VN", {
+   /* const formattedOpenTime = store?.open_time.toLocaleTimeString("vi-VN", {
         hour: "2-digit",
         minute: "2-digit",
     }); const formattedCloseTime = store?.close_time.toLocaleTimeString("vi-VN", {
         hour: "2-digit",
         minute: "2-digit",
-    });
+    });*/
     return (
         <Modal visible={visible} animationType="slide" >
             <View className="flex-1 bg-white">
                 {/* Ảnh */}
                 <Image
-                    source={{ uri: store?.image }}
+                    source={{ uri: store?.imageURL }}
                     className="w-full"
                     style={{ height: height * 0.5 }}
                 />
@@ -44,7 +44,7 @@ const ShopModal: React.FC<ShopModalProps> = ({ visible, onClose, store }) => {
                     <Text className="text-2xl mt-1 font-bold ">
                         {store?.address}
                     </Text>
-                    <Text className="text-gray-500 text-base mt-1">Giờ mở cửa: {formattedOpenTime} - {formattedCloseTime}</Text>
+                    <Text className="text-gray-500 text-base mt-1">Giờ mở cửa: 8:00 - 22:00</Text>
                     <View className={"flex-row items-center mt-5"} >
                         <Navigation size={24} color={"black"} />
                         <Text className=" text-lg ml-3">Cách đây {store?.distance} km</Text>
